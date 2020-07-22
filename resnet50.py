a114f9b7e0732036e33987972913def3edcb861c
Adesh Gautam <adeshgautam@Adeshs-MacBook-Air.local>
2020-07-22 11:36:16 +0530

    added resnet50

---
+++ resnet50.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+import keras
+
+from keras.layers import *
+
+def conv_block(base, filters, kernel_size, padding, strides, name):
+    x = Conv2D(filters, kernel_size=kernel_size, padding=padding, strides=strides, name='conv'+name)(base)
+    x = BatchNormalization(axis=3, epsilon=1.001e-5, name='batchnorm'+name)(x)
+    x = Activation('relu', name='act'+name)(x)
+    return x
+
+def resnet50(classes=classes):
+	inp = Input(shape=(224,224,3), name='input_layer')
+	x = Conv2D(64, kernel_size=(7,7), padding='valid', strides=(2,2), activation='relu', name='conv_0')(inp)
+	base = MaxPool2D(pool_size=(3,3), strides=(2,2), name='maxpool_0')(x)
+
+	print('Stage 0:', base.shape)
+
+	# Stage 1 (3 cnv_blocks)
+	names = ['_1_a', '_1_b', '_1_c']
+	for n in names:
+	    x = conv_block(base, 64, kernel_size=(1,1), padding='same', strides=(2,2), name=n+'1')
+	    x = conv_block(x, 64, kernel_size=(3,3), padding='same', strides=(1,1), name=n+'2')
+	    x1 = conv_block(x, 256, kernel_size=(1,1), padding='same', strides=(1,1), name=n+'3')
+	    
+	    shortcut = conv_block(base, 256, kernel_size=(1,1), padding='same', strides=(2,2), name=n+'4')
+	    base = Add(name='add'+n+'1')([x1, shortcut])
+
+	print('Stage 1:', base.shape)
+	    
+	# Stage 2 (4 cnv_blocks)
+	names = ['_2_a', '_2_b', '_2_c', '_2_d']
+	for n in names:
+	    x = conv_block(base, 128, kernel_size=(1,1), padding='same', strides=(2,2), name=n+'1')
+	    x = conv_block(x, 128, kernel_size=(3,3), padding='same', strides=(1,1), name=n+'2')
+	    x1 = conv_block(x, 512, kernel_size=(1,1), padding='same', strides=(1,1), name=n+'3')
+	    
+	    shortcut = conv_block(base, 512, kernel_size=(1,1), padding='same', strides=(2,2), name=n+'4')
+	    base = Add(name='add'+n+'1')([x1, shortcut])
+
+	print('Stage 2:', base.shape)
+	    
+	# Stage (6 cnv_blocks)
+	names = ['_3_a', '_3_b', '_3_c', '_3_d', '_3_e', '_3_f']
+	for n in names:
+	    x = conv_block(base, 256, kernel_size=(1,1), padding='same', strides=(2,2), name=n+'1')
+	    x = conv_block(x, 256, kernel_size=(3,3), padding='same', strides=(1,1), name=n+'2')
+	    x1 = conv_block(x, 1024, kernel_size=(1,1), padding='same', strides=(1,1), name=n+'3')
+	    
+	    shortcut = conv_block(base, 1024, kernel_size=(1,1), padding='same', strides=(2,2), name=n+'4')
+	    base = Add(name='add'+n+'1')([x1, shortcut])
+
+	print('Stage 3:', base.shape)
+
+	# Stage (3 cnv_blocks)
+	names = ['_4_a', '_4_b', '_4_c']
+	for n in names:
+	    x = conv_block(base, 512, kernel_size=(1,1), padding='same', strides=(2,2), name=n+'1')
+	    x = conv_block(x, 512, kernel_size=(3,3), padding='same', strides=(1,1), name=n+'2')
+	    x1 = conv_block(x, 2048, kernel_size=(1,1), padding='same', strides=(1,1), name=n+'3')
+	    
+	    shortcut = conv_block(base, 2048, kernel_size=(1,1), padding='same', strides=(2,2), name=n+'4')
+	    base = Add(name='add'+n+'1')([x1, shortcut])
+
+	print('Stage 4:', base.shape)
+
+	out = GlobalAveragePooling2D(name='global_avg_1')(base)
+	out = Dense(classes, activation='softmax', name='dense_1')(out)
+
+	model = keras.Model(inputs=inp, outputs=out, name="renset50_model")
+
+	opt = keras.optimizers.Adam(lr=0.001)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+	return model
